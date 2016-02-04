0b803dc2ac6e7c1590d3196cd103337e70a9b6ec
Thomas Phillips <tpphilli@amazon.com>
2016-02-03 17:15:28 -0800

    Added problem details to README.md Added JS setup code.

--- playground.js
+++ playground.js
@@ -1,3 +1,40 @@
 (function() {
   console.log("Hey IIFE");
+  //Setup
+
+  var contacts = [
+  {
+    "firstName": "Akira",
+    "lastName": "Laine",
+    "number": "0543236543",
+    "likes": ["Pizza", "Coding", "Brownie Points"]
+  },
+  {
+    "firstName": "Harry",
+    "lastName": "Potter",
+    "number": "0994372684",
+    "likes": ["Hogwarts", "Magic", "Hagrid"]
+  },
+  {
+    "firstName": "Sherlock",
+    "lastName": "Holmes",
+    "number": "0487345643",
+    "likes": ["Intriguing Cases", "Violin"]
+  },
+  {
+    "firstName": "Kristian",
+    "lastName": "Vos",
+    "number": "unknown",
+    "likes": ["Javascript", "Gaming", "Foxes"]
+  }
+];
+
+function lookUp(firstName, prop){
+// Only change code below this line
+
+// Only change code above this line
+}
+
+// Change these values to test your function
+lookUp("Akira", "likes");
 })();
